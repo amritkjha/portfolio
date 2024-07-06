831847683b305aca673d7bac1072b25ad7d55f6f
amritIkokas <amritkumarjha7777@gmail.com>
2024-07-06 14:37:10 +0530

    New project Update - Samvaad Space

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -84,30 +84,35 @@ export function Project() {
           </div>
         </ScrollAnimation>
 
-        {/* <ScrollAnimation animateIn="flipInX">
+        <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links">
-                <a href="https://justmedicalbooks.com/" target="_blank" rel="noreferrer">
+                <a href="https://github.com/amritkjha/samvaad-space" target="_blank" rel="noreferrer">
+                  <img src={githubIcon} alt="Visit site" /> </a>
+                <a href="https://samvaad-space.vercel.app/" target="_blank" rel="noreferrer">
                   <img src={externalLink} alt="Visit site" />
                 </a>
               </div>
             </header>
             <div className="body">
-              <h3>MedicalBooks - Book Selling Site</h3>
+              <h3>Samvaad Space - Professional Community Network</h3>
               <p>
-                This website sells medical books online and is popular with medical students. Medical students use it to buy books and acquire knowledge. The site has all kinds of medical books in one place.
+                Developed a platform tailored for professionals across various fields, enabling them to build communities and share expertise with members. For instance, stock market tips can be disseminated daily. Additionally, an upcoming subscription feature will allow authors to set their own subscription fees.
               </p>
             </div>
             <footer>
               <ul className="tech-list">
-                <li>Wordpress</li>
-                <li>Woocommerce</li>
+                <li>ReactJs</li>
+                <li>MongoDB</li>
+                <li>Tailwind</li>
+                <li>NodeJs</li>
+                <li>SocketIo</li>
               </ul>
             </footer>
           </div>
-        </ScrollAnimation> */}
+        </ScrollAnimation>
 
         {/* <ScrollAnimation animateIn="flipInX">
           <div className="project">
